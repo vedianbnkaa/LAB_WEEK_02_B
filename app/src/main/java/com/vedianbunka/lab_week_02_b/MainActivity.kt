ec1e644727d05e150dad46f809d099d64afdeabe
vedianbnka <v.bunka238@gmail.com>
2025-09-01 23:02:48 +0700

    Commit No. 2 – Result activity [Intent]

--- app/src/main/java/com/vedianbunka/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/vedianbunka/lab_week_02_b/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.vedianbunka.lab_week_02_b
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val COLOR_KEY = "COLOR_KEY"
+    }
+
+    private val submitButton : Button get() = findViewById(R.id.submit_button)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background_screen)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        submitButton.setOnClickListener {
+            val colorCode = findViewById<TextInputEditText>(R.id.color_code_input_field).text.toString()
+            if(colorCode.isNotEmpty()){
+                if(colorCode.length < 6){
+                    Toast.makeText(this, getString(R.string.color_code_input_wrong_length), Toast.LENGTH_LONG).show()
+                } else {
+                    val ResultIntent = Intent(this, ResultActivity::class.java)
+                    ResultIntent.putExtra(COLOR_KEY, colorCode)
+                    startActivity(ResultIntent)
+                }
+            } else {
+                Toast.makeText(this, getString(R.string.color_code_input_empty), Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/vedianbunka/lab_week_02_b/ResultActivity.kt
@@ -0,0 +1,35 @@
+package com.vedianbunka.lab_week_02_b
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    companion object{
+        private const val COLOR_KEY = "COLOR_KEY"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background_screen)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if(intent != null){
+            val colorCode = intent.getStringExtra(COLOR_KEY)
+
+            val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
+            backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
+            resultMessage.text = getString(R.string.color_code_result_message,colorCode?.uppercase())
+        }
+    }
+}
